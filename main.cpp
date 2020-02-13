5c91266af66743169f9d047d50beb8cf3fecedec
jacks <jheck@smu.edu>
2020-02-13 15:46:33 -0600

    Fixing catch2 Test to run in github

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ void populateJPVectorFromInput(JPVector<JPString*>&, ifstream&);
 int main(int argc, char** argv) {
     if(argc == 1) {
         Catch::Session().run();
+        return 0;
     }
     ifstream inFile(argv[1]);
     ifstream bookIn(argv[2]);
